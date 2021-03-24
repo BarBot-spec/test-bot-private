634a64dfc548c1db187c3a9120b5aa82e90d2de8
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-24 21:17:35 +0700

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch')
-const FormData = require('form-data')
+const uploadImage = require('../lib/uploadImage.js')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler  = async (m, { conn, text }) => {
@@ -8,7 +8,7 @@ let handler  = async (m, { conn, text }) => {
     let json = await res.json()
     let img = Buffer.from(json.base64.split`,`[1], 'base64')
     if (!img) throw img
-    let stiker = await sticker(img)
+    let stiker = await sticker(img, false, packname, author)
     conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
@@ -29,60 +29,3 @@ handler.botAdmin = false
 handler.fail = null
 
 module.exports = handler
-
-async function canvas(code, type = 'png', quality = 0.92) {
-    let res = await fetch('https://nurutomo.herokuapp.com/api/canvas?' + queryURL({
-        type,
-        quality
-    }), {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'text/plain',
-            'Content-Length': code.length
-        },
-        body: code
-    })
-    let image = await res.buffer()
-    return image
-}
-
-function queryURL(queries) {
-    return Object.entries(queries).map(([key, value]) => key + (value ? '=' + encodeURIComponent(value) : '')).join('&')
-}
-
-let { fromBuffer } = require('file-type')
-async function sticker(img, url) {
-    url = url ? url : await uploadImage(img)
-    let {
-        mime
-    } = url ? {mime:'image/jpeg'} : await fromBuffer(img)
-    let sc = `let im = await loadImg('data:${mime};base64,'+(await window.loadToDataURI('${url}')))
-c.width = c.height = 512
-let max = Math.max(im.width, im.height)
-let w = 512 * im.width / max
-let h = 512 * im.height / max
-ctx.drawImage(im, 256 - w / 2, 256 - h / 2, w, h)
-`
-    return await canvas(sc, 'webp')
-}
-
-function uploadImage(buffer) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const {
-                ext
-            } = await fromBuffer(buffer)
-            let form = new FormData()
-            form.append('file', buffer, 'tmp.' + ext)
-            let res = await fetch('https://telegra.ph/upload', {
-                method: 'POST',
-                body: form
-            })
-            let img = await res.json()
-            if (img.error) reject(img.error)
-            else resolve('https://telegra.ph' + img[0].src)
-        } catch (e) {
-            reject(e)
-        }
-    })
-}
