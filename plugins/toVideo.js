90430c230036a41af1e8360e02cb92af2f78b9a1
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-24 21:38:50 +0700

    Update toVideo.js

--- plugins/toVideo.js
+++ plugins/toVideo.js
@@ -1,7 +1,6 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { toVideo } = require('../lib/converter')
 let handler  = async (m, { conn, args }) => {
-  let stiker = false
   try {
     let q = m.quoted ? m.quoted : m
       let img = await q.download()
@@ -9,7 +8,7 @@ let handler  = async (m, { conn, args }) => {
 }
 handler.help = ['tovideo (reply stiker)']
 handler.tags = ['sticker']
-handler.command = /^tovideo?$/i
+handler.command = /^(tovideo)?$/i
 
 module.exports = handler
 
