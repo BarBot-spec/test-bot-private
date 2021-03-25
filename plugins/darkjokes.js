f27fbc6ca34db03cb16905b42a0138fd7731a999
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-25 19:44:40 +0700

    Update darkjokes.js

--- plugins/darkjokes.js
+++ plugins/darkjokes.js
@@ -1,6 +1,5 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...url nya mana?'
   let res = await fetch(`https://recoders-area.herokuapp.com/api/darkjokes?apikey=FreeApi`)
   let json = await res.json()
   conn.sendFile(m.chat, json.result, 'darkjokes.png', `......`, m, false)
