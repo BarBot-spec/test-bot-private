722197266b4a602ec76e1188023d1069c65f1066
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-25 22:30:27 +0700

    Update info.js

--- plugins/info.js
+++ plugins/info.js
@@ -1,12 +1,15 @@
-let handler = async m => m.reply(`
+let handler = async (m, { conn, text }) => {
+m.reply(`
 Informations About Me
 
-➠ Name : ${this.user.name}
+➠ Name : ${getName(conn.user.jid)}
 ➠ Developer : Caliph
 ➠ Language : Node.js
 ➠ Framework : Baileys
 ➠ Server : Heroku
+➠ Source : Nurutomo
 `.trim()) // Tambah sendiri kalo mau
+}
 handler.help = ['info']
 handler.tags = ['info']
 handler.command = /^info$/i
